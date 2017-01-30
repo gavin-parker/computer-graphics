6164443caac7dc4a9fb64300224bbc6032a1ab5d
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-01-30 10:51:07 +0000

    added interpolation for vec3

--- src/skeleton.cpp
+++ src/skeleton.cpp
@@ -22,6 +22,7 @@ int t;
 void Update();
 void Draw();
 void Interpolate(float a, float b, vector<float>& result);
+void Interpolate(vec3 a, vec3 b, vector<vec3>& result);
 
 int main()
 {
@@ -62,10 +63,11 @@ void Draw()
 
 	for( int y=0; y<SCREEN_HEIGHT; ++y )
 	{
+
 		for( int x=0; x<SCREEN_WIDTH; ++x )
 		{
-			vec3 color( 0.0, 1.0, 0.0 );
-			PutPixelSDL( screen, x, y, color );
+			//vec3 color( 0.0, 1.0, 0.0 );
+			PutPixelSDL( screen, x, y, result[x] );
 		}
 	}
 
@@ -84,5 +86,17 @@ void Interpolate(float a, float b, vector<float>& result){
 	float step = (b + 1 - a) / result.size();
 	for(unsigned int i=0; i < result.size(); i++){
 		result[i] = a + step*i;
+	}
+}
+
+//Linearly interpolate between two vectors
+void Interpolate(vec3 a, vec3 b, vector<vec3>& result){
+	if(result.size() == 1){
+		result[0] = vec3((a[0] + b[0]/2), (a[1] + b[1]/2), (a[2] + b[2]/2));
+		return;
+	}
+	vec3 step((b[0] + 1 - a[0]) / result.size(), (b[1] + 1 - a[1]) / result.size(), (b[2] + 1 - a[2]) / result.size());
+	for(unsigned int i = 0; i < result.size(); i++){
+		result[i] = a + step* (float)i;
 	}
 }
