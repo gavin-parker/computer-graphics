0ec1a6317752c8f9e02ed9219341cc2daf09f6b6
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-02-01 17:28:48 +0000

    Camera now hides its fields

--- include/camera.h
+++ include/camera.h
@@ -14,12 +14,13 @@ struct Ray {
 };
 
 class Camera {
-public:
+private:
 	vec3 position;
 
 	const float viewOffset;
 	const float velocity = 1.0f;
 
+public:
 	Camera(vec3 position, float viewAngle);
 
 	void Update(float dt);
