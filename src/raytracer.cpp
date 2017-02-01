bf26af56b0845ee3e8d893c18f184a7e275b6c49
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-02-01 13:38:43 +0000

    moved camera to sensible location

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -2,7 +2,7 @@
 
 RayTracer::RayTracer(int width, int height,  bool fullscreen):
 	SdlScreen(width, height, fullscreen){
-    camera = vec3(0,0,0);
+    camera = vec3(0,0,-1);
 	triangles = LoadTestModel();
 }
 
@@ -23,7 +23,7 @@ void RayTracer::draw() {
     }
 }
 
-bool RayTracer::ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle> &triangles, Intersection& closestIntersection){
+bool RayTracer::ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle> &triangles, Intersection &closestIntersection){
     closestIntersection.distance = numeric_limits<float>::max();
     for(size_t i = 0; i < triangles.size(); i++){
         Triangle triangle = triangles[i];
@@ -33,7 +33,7 @@ bool RayTracer::ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>
         glm::mat3 A(-dir, e1, e2);
         vec3 x = glm::inverse(A)*b;
         //bit dodge
-        if(x[1] > 0 && x[2] > 0 && (x[1] + x[2]) < 1 && x[0] >= 0 && x[0] < closestIntersection.distance){
+        if(x[1] >= 0 && x[2] >= 0 && (x[1] + x[2]) < 1 && x[0] >= 0 && x[0] < closestIntersection.distance){
             closestIntersection.distance = x[0];
             closestIntersection.triangleIndex = i;
             closestIntersection.position = x;
