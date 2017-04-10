e8fbb2ef031e65cfb3839510d6b1529bfabef41d
Gavin Parker <gp14958@my.bristol.ac.uk>
2017-04-10 15:53:27 +0100

    use merl database code

--- include/brdf.h
+++ include/brdf.h
@@ -7,7 +7,7 @@ private:
 
 public:
 	//returns the reflection
-	vec3 getLight(vec3 incident, vec3 view);
+	vec3 getLight(vec3 incident, vec3 view, vec3 normal);
 	
 	//microfacet distribution function
 	vec3 mdf(float angle);
--- src/brdf.cpp
+++ src/brdf.cpp
@@ -1,11 +1,35 @@
 #include "brdf.h"
 BRDF::BRDF(vec3 diffuse) : diffuse(diffuse) {};
 
+using glm::length;
+
+float angleBetween(vec3 a, vec3 b) {
+	return acosf(dot(a, b) / length(a)*length(b));
+}
 
 //attempt at disney BRDF model https://disney-animation.s3.amazonaws.com/library/s2012_pbs_disney_brdf_notes_v2.pdf
-vec3 BRDF::getLight(vec3 incident, vec3 view) {
+vec3 BRDF::getLight(vec3 incident, vec3 view, vec3 normal) {
 	vec3 h = (incident + view) / glm::length(incident + view);
-
-
-
+	float thetaL = angleBetween(incident, normal);
+	float thetaV = angleBetween(view, normal);
+	float thetaH = angleBetween(normal, h);
+	float thetaD = angleBetween(incident, h);
+	return diffuse + (mdf(thetaH)*frc(thetaD)*sf(thetaL, thetaD)) * (1.f/ 4.f*cos(thetaL)*cos(thetaV));
 }
+
+
+//microfacet distribution function
+vec3 BRDF::mdf(float angle) {
+	return vec3(0, 0, 0);
+}
+
+//fresnel reflection coefficient
+vec3 BRDF::frc(float angle) {
+	return vec3(0, 0, 0);
+}
+
+//shadowing factor
+vec3 BRDF::sf(float incidence, float view) {
+	return vec3(0, 0, 0);
+}
+
